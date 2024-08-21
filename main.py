878ef05de2619871adad15f562da77b18795b0e2
usman <usmanmajeed8063@gmail.com>
2024-08-21 17:21:04 +0500

    git

---
+++ main.py
@@ -0,0 +1,6 @@
+height=int(input("Enter your weight in kilograms: "))
+radius=int(input("Enter tour radius in meter: "))
+pai=float(input("Enter your pai: "))
+volume=(3.14),(radius**2),(height)
+print("volume")
+           
